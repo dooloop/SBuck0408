f6a98102fffac2a5eecec9c1ef012029662af214
Minug Kang <imuroox@gmail.com>
2017-04-08 15:15:50 +0900

    Initial Commit

---
+++ SBuck0408/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SBuck0408
+//
+//  Created by Minug Kang on 2017. 4. 8..
+//  Copyright © 2017년 Minug Kang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
